83c1dec4fd5122dbab35aa17d66b2af80f83d2a7
Bijaya Bhaskar Swain <sbijayabhaskar@gmail.com>
2019-01-25 13:14:54 +0530

    Initial import

---
+++ spring-security-auth-server/src/main/java/com/bhaskar/security/springsecurityauthserver/resource/HelloResource.java
@@ -0,0 +1,17 @@
+package com.bhaskar.security.springsecurityauthserver.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloResource {
+
+	@GetMapping
+	public String hello() {
+		System.out.println(" Inside geMapping Hello");
+		return "Hello World";
+	}
+
+}
